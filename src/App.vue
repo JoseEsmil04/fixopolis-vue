46f12035518e486de20393e66f133cc98b5c3128
JoseEsmil04 <josecampusano0427@gmail.com>
2025-03-28 21:25:26 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex h-screen justify-center flex-col">
+    <h1 class="underline text-5xl text-cyan-800 font-bold text-center">Fixopolis Ferreteria</h1>
+  </div>
+</template>
